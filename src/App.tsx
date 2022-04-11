46bda9521634549e59bf1eb6beb7fabfa89ce7a5
Vladislav Oceretnij <vladislav.oceretnij@wdf.cz>
2022-04-11 22:21:03 +0200

    feat: setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import User from 'pages/user';
+import Users from 'pages/users';
 import './App.css';
 
 function App() {
   return (
-    <></>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <Users /> } />
+        <Route path="/user/:id" element={ <User /> } />
+      </Routes>
+    </Router>
   );
 }
 
